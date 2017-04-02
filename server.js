65c2019e092fa404dd842c53b72965108f905e9e
Jan Rychlik <jan.rychlik@improvisio.cz>
2017-04-02 19:48:06 +0200

    modified package json and server js for the heroku

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
+
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
@@ -43,6 +45,6 @@ app.get('/', function(req, res) {
   });
 });
 
-app.listen(3000, function() {
-  console.log('server is up on port 3000');
+app.listen(port, function() {
+  console.log('server is up on port ' + port);
 });
