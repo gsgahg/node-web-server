90e31f9c93cb5f481c54505bd5dab1b72ecb4056
Jan Rychlik <jan.rychlik@improvisio.cz>
2017-04-02 17:06:28 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+
+
+app.use(express.static(__dirname + '/public'));
+app.use(function(req, res, next) {
+  var now = new Date().toString();
+  console.log(now + ': ' + req.method + req.url);
+  next();
+});
+
+hbs.registerHelper('getCurrentYear', function() {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', function(text) {
+  return text.toUpperCase();
+});
+
+app.get('/about', function(req, res) {
+  res.render('about.hbs', {
+    pageTitle: 'About page'
+  });
+});
+
+app.get('/bad', function(req, res) {
+  res.send({
+    error: 'bad request'
+  });
+});
+
+app.get('/', function(req, res) {
+  // res.send('<h1>hello express</h1>');
+  res.render('home.hbs', {
+    pageTitle: 'Home page',
+    welcomeMessage: 'welcome to the home page'
+  });
+});
+
+app.listen(3000, function() {
+  console.log('server is up on port 3000');
+});
